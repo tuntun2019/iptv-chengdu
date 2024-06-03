2cdaea6ee17f76ad3ae10bf631dc5ca3b79c8e42
tuntun2019 <53038778+tuntun2019@users.noreply.github.com>
2024-06-03 23:07:33 +0800

    Update home.py

--- script/home.py
+++ script/home.py
@@ -16,7 +16,7 @@ import re
 sourceTvboxIptv="https://raw.githubusercontent.com/gaotianliuyun/gao/master/list.txt"
 sourceIcon51ZMT="http://epg.51zmt.top:8000"
 sourceChengduMulticast="http://epg.51zmt.top:8000/sctvmulticast.html"
-homeLanAddress="http://192.168.42.254:6688"
+homeLanAddress="http://192.168.42.254:24242"
 
 groupCCTV=["CCTV", "CETV", "CGTN"]
 groupWS=[ "卫视"]
